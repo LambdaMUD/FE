996b3fc229955e40410c98fe1d9537b7ebea3106
david-chua <dp_chua@hotmail.com>
2019-09-21 16:21:49 -0400

    started front end infrastruture

---
+++ lambdamud_fe/src/Components/GameBoard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class GameBoard extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1> Game Board </h1>
+      </div>
+    )
+  }
+}
+
+export default GameBoard;
