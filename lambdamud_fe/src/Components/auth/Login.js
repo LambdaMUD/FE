eb3b85cc29a5f78494e3a1f0cb492d34a59d3a85
Leila <nb.leila10@gmail.com>
2019-09-22 17:50:35 -0500

    Implement login page
    route: /login

--- lambdamud_fe/src/Components/auth/Login.js
+++ lambdamud_fe/src/Components/auth/Login.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Card, Button, Form } from "react-bootstrap";
 
-const Login = () => {
+const Login = props => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = event => {
+    event.preventDefault();
+    logUser({ username, password });
+  };
+
+  const logUser = user => {
+    const endpoint = "https://lambdamud-be.herokuapp.com/api/";
+    axios.post(endpoint, user).then(response => {
+      console.log(response.data);
+      localStorage.setItem("token", response.data.token);
+      props.history.push("/");
+    });
+  };
   return (
     <Card style={{ width: "70%" }} className='Register'>
       <Card.Header as='h5'>Login</Card.Header>
       <Card.Body>
-        <Form>
+        <Form onSubmit={loginHandler}>
           <Form.Group controlId='formBasicEmail'>
             <Form.Label>User Name</Form.Label>
-            <Form.Control name='username' type='text' placeholder='Enter your user name' />
+            <Form.Control
+              type='text'
+              placeholder='Enter your user name'
+              name='username'
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
           </Form.Group>
           <Form.Group controlId='formBasicPassword'>
             <Form.Label>Password</Form.Label>
-            <Form.Control name='password1' type='password' placeholder='Password' />
+            <Form.Control
+              name='password1'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
           </Form.Group>
           <Button variant='primary' type='submit'>
             Submit
