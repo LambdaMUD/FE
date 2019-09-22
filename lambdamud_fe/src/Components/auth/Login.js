21ff4796234d6e2e77e5c4d75391bcd8072325b2
Leila <nb.leila10@gmail.com>
2019-09-22 18:29:06 -0500

    Add error handling for the login/register page

--- lambdamud_fe/src/Components/auth/Login.js
+++ lambdamud_fe/src/Components/auth/Login.js
@@ -6,6 +6,8 @@ const Login = props => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const [ErrorText, setErrorText] = useState("");
+
   const loginHandler = event => {
     event.preventDefault();
     logUser({ username, password });
@@ -13,43 +15,62 @@ const Login = props => {
 
   const logUser = user => {
     const endpoint = "https://lambdamud-be.herokuapp.com/api/";
-    axios.post(endpoint, user).then(response => {
-      console.log(response.data);
-      localStorage.setItem("token", response.data.token);
-      props.history.push("/");
-    });
+    axios
+      .post(endpoint, user)
+      .then(response => {
+        console.log(response.data);
+        localStorage.setItem("token", response.data.token);
+        setUsername("");
+        setPassword("");
+        setErrorText("");
+        props.history.push("/");
+      })
+      .catch(error => {
+        console.log(error.response.data);
+        const errors = Object.keys(error.response.data);
+        let message = "";
+        if (errors[0]) {
+          if (errors[0] !== "non_field_errors") message = errors[0] + " : " + error.response.data[errors[0]];
+          else message = error.response.data[errors[0]];
+          setErrorText(message);
+        }
+        props.history.push("/login");
+      });
   };
   return (
-    <Card style={{ width: "70%" }} className='Register'>
-      <Card.Header as='h5'>Login</Card.Header>
-      <Card.Body>
-        <Form onSubmit={loginHandler}>
-          <Form.Group controlId='formBasicEmail'>
-            <Form.Label>User Name</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='Enter your user name'
-              name='username'
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId='formBasicPassword'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              name='password1'
-              type='password'
-              placeholder='Password'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Button variant='primary' type='submit'>
-            Submit
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
+    <>
+      {ErrorText && <div>{ErrorText}</div>}
+      <Card style={{ width: "70%" }} className='Register'>
+        <Card.Header as='h5'>Login</Card.Header>
+        <Card.Body>
+          <Form onSubmit={loginHandler}>
+            <Form.Group controlId='formBasicEmail'>
+              <Form.Label>User Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter your user name'
+                name='username'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='formBasicPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name='password1'
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
--- lambdamud_fe/src/Components/auth/register.js
+++ lambdamud_fe/src/Components/auth/register.js
@@ -7,6 +7,8 @@ const Register = props => {
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
 
+  const [ErrorText, setErrorText] = useState("");
+
   const registerHandler = event => {
     event.preventDefault();
     const newUser = {
@@ -20,55 +22,75 @@ const Register = props => {
 
   const addUser = user => {
     const endpoint = "https://lambdamud-be.herokuapp.com/api/registration/";
-    axios.post(endpoint, user).then(response => {
-      console.log(response.data);
-      props.history.push("/login");
-    });
+    axios
+      .post(endpoint, user)
+      .then(response => {
+        console.log(response.data);
+        setUsername("");
+        setPassword1("");
+        setPassword2("");
+        setErrorText("");
+        props.history.push("/login");
+      })
+      .catch(error => {
+        console.log(error.response.data);
+        const errors = Object.keys(error.response.data);
+        let message = "";
+        if (errors[0]) {
+          if (errors[0] !== "non_field_errors") message = errors[0] + " : " + error.response.data[errors[0]];
+          else message = error.response.data[errors[0]];
+          setErrorText(message);
+        }
+        props.history.push("/register");
+      });
   };
 
   return (
-    <Card style={{ width: "70%" }} className='Register'>
-      <Card.Header as='h5'>Register</Card.Header>
-      <Card.Body>
-        <Form onSubmit={registerHandler}>
-          <Form.Group controlId='formBasicText'>
-            <Form.Label>User Name</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='Enter your user name'
-              name='username'
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId='formBasicPassword1'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type='password'
-              placeholder='Password'
-              name='password1'
-              value={password1}
-              onChange={e => setPassword1(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId='formBasicPassword2'>
-            <Form.Label>Password Again</Form.Label>
-            <Form.Control
-              type='password'
-              placeholder='Type Password again'
-              name='password2'
-              value={password2}
-              onChange={e => setPassword2(e.target.value)}
-            />
-            <Form.Text className='text-muted'>The passwords have to match.</Form.Text>
-          </Form.Group>
+    <>
+      {ErrorText && <div>{ErrorText}</div>}
+      <Card style={{ width: "70%" }} className='Register'>
+        <Card.Header as='h5'>Register</Card.Header>
+        <Card.Body>
+          <Form onSubmit={registerHandler}>
+            <Form.Group controlId='formBasicText'>
+              <Form.Label>User Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter your user name'
+                name='username'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='formBasicPassword1'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                name='password1'
+                value={password1}
+                onChange={e => setPassword1(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId='formBasicPassword2'>
+              <Form.Label>Password Again</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Type Password again'
+                name='password2'
+                value={password2}
+                onChange={e => setPassword2(e.target.value)}
+              />
+              <Form.Text className='text-muted'>The passwords have to match.</Form.Text>
+            </Form.Group>
 
-          <Button variant='primary' type='submit'>
-            Submit
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
