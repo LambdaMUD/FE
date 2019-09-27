693c8711b4e14e7a8b8a31a1550f9d740f77131c
KieranVieira <kieranvieira@live.com>
2019-09-26 23:02:07 -0400

    almost finished chat

--- lambdamud_fe/src/Components/GameInfo.js
+++ lambdamud_fe/src/Components/GameInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PlayerStats from './PlayerStats';
 import PlayerController from './playerControls/PlayerController.jsx';
+import ChatRoom from './chat/ChatRoom.jsx';
 
 const GameInfo = () => {
   return(
@@ -8,6 +9,7 @@ const GameInfo = () => {
       <h1> Game Info </h1>
       <PlayerController/>
       <PlayerStats />
+      <ChatRoom/>
     </div>
   )
 }
